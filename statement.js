ed40e86c7fef9479902ddac19e7c49f9e29b539b
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-07-07 15:15:55 +0300

    fixed matjax

--- statement.js
+++ statement.js
@@ -1,11 +1,2 @@
 $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
-
-var head = document.getElementsByTagName("head")[0], script;
-script = document.createElement("script");
-script.type = "text/x-mathjax-config";
-script[(window.opera ? "innerHTML" : "text")] =
-    "MathJax.Hub.Config({\n" +
-    "  tex2jax: { inlineMath: [['$$$','$$$'], ['\\\\(','\\\\)']] }\n" +
-    "});";
-head.appendChild(script);
-MathJax.Hub.Typeset()
+MathJax.Hub.Typeset();
